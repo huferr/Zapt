2085d69c3aa9e2fcbb66ea1eab8923545015585c
Huferr <hugoferreirait@gmail.com>
2021-07-13 18:25:59 -0300

    feat: Sidebar Menu

---
+++ scripts/sidebar.js
@@ -0,0 +1,17 @@
+const menuButton = document.getElementById('hamburguer-menu-container');
+const backgroundMenu = document.getElementById('background-menu');
+const sidebarMenu = document.getElementById('sidebar-menu');
+const sidebarInfo = document.getElementById('sidebar-list');
+
+menuButton.addEventListener('click', () => {
+    backgroundMenu.style.visibility = 'visible'
+    backgroundMenu.style.opacity = 1
+    sidebarMenu.classList.add('active')
+})
+
+backgroundMenu.addEventListener('click', () => {
+    backgroundMenu.style.visibility = 'hidden'
+    backgroundMenu.style.opacity = 0
+    sidebarMenu.classList.remove('active')
+    
+})
