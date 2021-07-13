19853d0c0f92e6d9b8dd0b6e0cb57f9fc215e071
Huferr <hugoferreirait@gmail.com>
2021-07-13 18:28:13 -0300

    Code organization

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,17 +1,15 @@
-const menuButton = document.getElementById('hamburguer-menu-container');
-const backgroundMenu = document.getElementById('background-menu');
-const sidebarMenu = document.getElementById('sidebar-menu');
-const sidebarInfo = document.getElementById('sidebar-list');
+const menuButton = document.getElementById("hamburguer-menu-container");
+const backgroundMenu = document.getElementById("background-menu");
+const sidebarMenu = document.getElementById("sidebar-menu");
 
-menuButton.addEventListener('click', () => {
-    backgroundMenu.style.visibility = 'visible'
-    backgroundMenu.style.opacity = 1
-    sidebarMenu.classList.add('active')
-})
+menuButton.addEventListener("click", () => {
+  backgroundMenu.style.visibility = "visible";
+  backgroundMenu.style.opacity = 1;
+  sidebarMenu.classList.add("active");
+});
 
-backgroundMenu.addEventListener('click', () => {
-    backgroundMenu.style.visibility = 'hidden'
-    backgroundMenu.style.opacity = 0
-    sidebarMenu.classList.remove('active')
-    
-})
+backgroundMenu.addEventListener("click", () => {
+  backgroundMenu.style.visibility = "hidden";
+  backgroundMenu.style.opacity = 0;
+  sidebarMenu.classList.remove("active");
+});
